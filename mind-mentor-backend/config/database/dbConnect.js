b59420cef3c1419083d49b1e680400f47582171a
Aswinrajr <aswinrajr07@gmail.com>
2025-05-12 12:30:00 +0530

    rehosting

--- mind-mentor-backend/config/database/dbConnect.js
+++ mind-mentor-backend/config/database/dbConnect.js
@@ -7,8 +7,8 @@ const dbConnect = () => {
   mongoose
     .connect(
           
-          // `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.jczad.mongodb.net/${MONGO_DATABASE_NAME}`
-          `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.f5db1.mongodb.net/${MONGO_DATABASE_NAME}`
+          `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.jczad.mongodb.net/${MONGO_DATABASE_NAME}`
+          // `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.f5db1.mongodb.net/${MONGO_DATABASE_NAME}`
           
       , {
       serverSelectionTimeoutMS: 30000, 
