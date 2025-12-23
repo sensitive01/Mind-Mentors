762e979acd08118980e6d0cf4f6619c32a8ec53f
Aswinrajr <aswinrajr07@gmail.com>
2025-12-24 03:53:11 +0530

    trst

--- mind-mentor-backend/config/database/dbConnect.js
+++ mind-mentor-backend/config/database/dbConnect.js
@@ -1,3 +1,27 @@
+// const mongoose = require("mongoose");
+
+// const dbConnect = () => {
+//   console.log("Welcome to database");
+
+//   mongoose
+//     .connect(
+//       process.env.MONGO_URI,
+//       {
+//         serverSelectionTimeoutMS: 30000,
+//         socketTimeoutMS: 45000,
+//       }
+//     )
+//     .then(() => {
+//       console.log("Connected to the database Atlas");
+//     })
+//     .catch((err) => {
+//       console.error("Error in connecting the database", err);
+//     });
+// };
+
+// module.exports = dbConnect;
+
+
 const mongoose = require("mongoose");
 
 const dbConnect = () => {
@@ -5,7 +29,7 @@ const dbConnect = () => {
 
   mongoose
     .connect(
-      process.env.MONGO_URI,
+        "mongodb+srv://MindMentorz:3jQhR36LMPNNznsN@cluster0.f5db1.mongodb.net/mindmentors",
       {
         serverSelectionTimeoutMS: 30000,
         socketTimeoutMS: 45000,
